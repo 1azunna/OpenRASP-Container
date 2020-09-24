1d8285213f732c16c14281efd4b8935a9725221c
Azunna Ikonne <aikonne@getcarbon.co>
2020-09-24 23:39:52 +0100

    Created Elasticsearch user and Script

--- elasticsearch/create_user.sh
+++ elasticsearch/create_user.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+bin/elasticsearch-keystore create -p $ELASTIC_ROOT_PASSWORD
 bin/elasticsearch-setup-passwords auto
 bin/elasticsearch-users useradd $ELASTIC_USER -p $ELASTIC_PASSWORD -r reporting_user,remote_monitoring_agent,monitoring_user,ingest_admin
