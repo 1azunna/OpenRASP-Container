201cf8a29f74836da861ba075958bf9ecee95af5
Azunna Ikonne <aikonne@getcarbon.co>
2020-09-25 14:44:50 +0100

    First working commit

--- start.sh
+++ start.sh
@@ -2,31 +2,6 @@
 set -x
 pid=0
 
-# mongo_check () {
-# while true;
-# do
-#   curl -s mongo:27017
-#   if [ $? -eq 0 ];
-#   then 
-#     echo OK
-#     break
-#   fi
-# done
-# }
-
-# elastic_check () {
-# while true;
-# do
-#   curl -s elasticsearch:9200
-#   if [ $? -eq 0 ];
-#   then 
-#     echo OK
-#     break
-#   fi
-# done
-# }
-
-
 # SIGUSR1-handler
 my_handler() {
   echo "my_handler"
@@ -46,11 +21,10 @@ term_handler() {
 trap 'kill ${!}; my_handler' SIGUSR1
 trap 'kill ${!}; term_handler' SIGTERM
 
-./rasp-cloud -d & 
+./rasp-cloud -d && sleep 15s; ln -sf /dev/stdout /rasp/logs/api/agent-cloud.log &
 pid="$!"
 # wait forever
 while true
 do
-  ln -sf /dev/stdout /rasp/logs/api/agent-cloud.log
-  tail -f /dev/null & wait ${!}
+  tail -f /dev/stdout & wait ${!}
 done
